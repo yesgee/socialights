79297c1f809fd923c2ec2501fabc6b2abc1f0c7b
Hylke Visser <htdvisser@gmail.com>
2015-03-03 15:53:15 +0200

    Fix createTeam action and tests

--- actions/games/createTeam.js
+++ actions/games/createTeam.js
@@ -9,60 +9,73 @@ exports.createTeam = {
   },
   inputs: {
     user: {
-      required: true,
-      formatter: function(s){ return String(s); }
+      required: false,
+      formatter: function(s) { return String(s); }
     },
     game: {
       required: true,
-      formatter: function(s){ return String(s); }
+      formatter: function(s) { return String(s); }
     },
     teamName: {
       required: true,
-      formatter: function(s){ return String(s); }
+      formatter: function(s) { return String(s); }
     },
     teamColor: {
       required: true,
-      formatter: function(s){ return String(s); }
+      formatter: function(s) { return String(s); }
     }
   },
   run: function(api, connection, next) {
-    var userId = connection.params.user;
-    var gameId = connection.params.game;
+
+    var gameId = new api.mongo.ObjectID(connection.params.game);
     var teamName = connection.params.teamName;
     var teamColor = connection.params.teamColor;
-    api.models.Game.findById(gameId, function(err, game){
-      if(err)
-      {
-        connection.response.success = false;
+
+    var team = {
+      name: teamName,
+      color: teamColor,
+      users: []
+    };
+
+    api.models.Game.findById(gameId, function(err, game) {
+      if (err) {
         connection.response.error = err;
         next(connection, true);
+      } else if (game === null) {
+        connection.response.error = 'Error: Game with this id was not found.';
+        next(connection, true);
       } else {
-        api.models.User.findById(userId, function(err2, user){
-          if(err2)
-          {
-            connection.response.success = false;
-            connection.response.error = err2;
-            next(connection, true);
-          } else{
-            game.teams.push({
-              name: teamName,
-              color: teamColor,
-              users: [user]
-            });
-            game.save(function(err3){
-              if(err3)
-              {
-                connection.response.success = false;
-                connection.response.error = err3;
-              }
-              else
-              {
-                connection.response.success = true;
-              }
+        var doCreateTeam = function(team) {
+          game.teams.push(team);
+          game.save(function(err, response) {
+            if (err) {
+              connection.response.success = false;
+              connection.response.error = err;
               next(connection, true);
-            });
-          }
-        });
+            }  else {
+              connection.response.success = true;
+              next(connection, true);
+            }
+          });
+        };
+
+        if (connection.params.user) {
+          var userId = new api.mongo.ObjectID(connection.params.user);
+          api.models.User.findById(userId, function(err, user) {
+            if (err) {
+              connection.response.error = err;
+              next(connection, true);
+            } else if (user === null) {
+              connection.response.error = 'Error: User with this id was not found.';
+              next(connection, true);
+            } else {
+              team.users.push(user._id);
+              doCreateTeam(team);
+            }
+          });
+        } else {
+          doCreateTeam(team);
+        }
       }
     });
   }
--- test/actions/games/createTeam.js
+++ test/actions/games/createTeam.js
@@ -12,15 +12,21 @@ var actionheroPrototype = require('actionhero').actionheroPrototype;
 var actionhero = new actionheroPrototype();
 var api;
 
+var randomGame = require('../../models/fixtures').Game;
+var randomUser = require('../../models/fixtures').User;
+
 describe('Action: createTeam', function() {
 
   before(function(done) {
     actionhero.start(function(err, a) {
       api = a;
       api.mongo.connect(function(err, db) {
-        // Remove all existing games
-        api.mongo.db.collection('games').remove(function() {
-          done();
+        // Remove all existing users
+        api.mongo.db.collection('users').remove(function() {
+          // Remove all existing games
+          api.mongo.db.collection('games').remove(function() {
+            done();
+          });
         });
       });
     });
@@ -32,8 +38,92 @@ describe('Action: createTeam', function() {
     });
   });
 
-  it('should require a .. parameter');
+  var user;
+  var game;
 
-  it('should create a team and save it to the database');
+  beforeEach(function(done) {
+    user = api.models.User(randomUser());
+    user.save(function(err, result) {
+      game = api.models.Game(randomGame());
+      game.save(function(err, result) {
+        done();
+      });
+    });
+  });
+
+  it('should return an error for a non-existing game', function(done) {
+    api.specHelper.runAction('createTeam', {
+      game: new api.mongo.ObjectID().toString(),
+      teamName: 'Team 404',
+      teamColor: '#ff0000'
+    }, function(response) {
+      should.exist(response.error);
+      should.not.exist(response.success);
+      response.error.should.equal('Error: Game with this id was not found.');
+      done();
+    });
+  });
+
+  it('should return an error for a non-existing user', function(done) {
+    api.specHelper.runAction('createTeam', {
+      user: new api.mongo.ObjectID().toString(),
+      game: game._id.toString(),
+      teamName: 'Blue Team',
+      teamColor: '#0000ff'
+    }, function(response) {
+      should.exist(response.error);
+      should.not.exist(response.success);
+      response.error.should.equal('Error: User with this id was not found.');
+      done();
+    });
+  });
+
+  it('should create a team without a user and save it to the database', function(done) {
+    api.specHelper.runAction('createTeam', {
+      game: game._id.toString(),
+      teamName: 'Blue Team',
+      teamColor: '#0000ff'
+    }, function(response) {
+      should.not.exist(response.error);
+      should.exist(response.success);
+
+      api.models.Game.findById(game._id, function(err, result) {
+        should.not.exist(err);
+
+        result.teams.should.not.be.empty;
+
+        result.teams[0].name.should.equal('Blue Team');
+        result.teams[0].color.should.equal('#0000ff');
+        result.teams[0].users.should.be.empty;
+
+        done();
+      });
+    });
+  });
+
+  it('should create a team with a user and save it to the database', function(done) {
+    api.specHelper.runAction('createTeam', {
+      game: game._id.toString(),
+      user: user._id.toString(),
+      teamName: 'Blue Team',
+      teamColor: '#0000ff'
+    }, function(response) {
+      should.not.exist(response.error);
+      should.exist(response.success);
+
+      api.models.Game.findById(game._id, function(err, result) {
+        should.not.exist(err);
+
+        result.teams.should.not.be.empty;
+
+        result.teams[0].name.should.equal('Blue Team');
+        result.teams[0].color.should.equal('#0000ff');
+        result.teams[0].users.should.not.be.empty;
+        result.teams[0].users.should.include(user._id);
+
+        done();
+      });
+    });
+  });
 
 });
