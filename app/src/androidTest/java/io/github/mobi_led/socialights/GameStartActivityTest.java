1d91332e66ff2f44cf269bb5d9e5b8587cda761d
samuelbalcha <samuel.balcha@gmail.com>
2015-03-05 14:19:48 +0200

    Testing Gradle and Travis setup

---
+++ app/src/androidTest/java/io/github/mobi_led/socialights/GameStartActivityTest.java
@@ -0,0 +1,47 @@
+package io.github.mobi_led.socialights;
+
+import android.app.Application;
+import android.content.Intent;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.ActivityUnitTestCase;
+import android.test.ApplicationTestCase;
+import android.widget.Button;
+
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class GameStartActivityTest extends ActivityInstrumentationTestCase2<GameStartActivity> {
+
+    GameStartActivity mainActivity;
+     Button testButton;
+
+    public GameStartActivityTest() {
+        super(GameStartActivity.class);
+    }
+
+    public void setUp() throws Exception{
+        super.setUp();
+
+
+        mainActivity = getActivity();
+
+        testButton = (Button)mainActivity.findViewById(R.id.test_button);
+
+
+    }
+
+    //Garbage collection
+    @Override
+    public void tearDown() throws Exception{
+       super.tearDown();
+    }
+
+    public void testRun(){
+
+        assertEquals(testButton.getText().toString(), "Test");
+
+    }
+
+
+}
