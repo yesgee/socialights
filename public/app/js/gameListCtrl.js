83a9304bc4a38a16332394634ae8e752323736f6
Jan van de Kerkhof <vdkerkhof.j@gmail.com>
2015-03-23 18:09:54 +0200

    Add game functionality

--- public/app/js/gameListCtrl.js
+++ public/app/js/gameListCtrl.js
@@ -3,11 +3,130 @@
 angular.module('adminControllers').controller('GameListCtrl', ['$scope', function($scope) {
   $scope.games = [];
   $scope.game = {};
+  $scope.allUsers = {};
+  $scope.currentTeam = {};
+  $scope.userId = null;
+
+  $scope.getUsers = function() {
+    client.action('listUsers', {}, function(err, data) {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        $scope.allUsers = data.users;
+        $scope.$digest();
+      }
+    });
+  };
+
+  $scope.getUsers();
+
+  $scope.toggleUserList = function(team) {
+    $scope.currentTeam = team;
+    $('#addUserModal').modal('show');
+  };
+
+  $scope.toggleTeamEdit = function(team) {
+    $scope.currentTeam = team;
+    $('#teamModal').modal('show');
+  };
+
+  $scope.saveTeam = function() {
+
+    client.action('updateTeam', {
+      game: $scope.selectedGame.id,
+      team: $scope.selectedGame.teams.indexOf($scope.currentTeam),
+      name: $scope.currentTeam.name,
+      color: $scope.currentTeam.color
+    }, function(err, data) {
+      if (data.error) {
+        alert(data.error);
+      } else {
+        $scope.selectedGame.teams = data.game.teams;
+        $('#teamModal').modal('hide');
+        $scope.getGames();
+      }
+    });
+  };
+
+  $scope.switchUserBetweenTeams = function() {
+    console.log($scope.userId);
+    client.action('switchUserBetweenTeams', {user: $scope.userId, game: $scope.selectedGame.id}, function(err, data) {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        $scope.selectedGame.teams = data.game.teams;
+        $scope.getGames();
+      }
+    });
+  };
+
+  $scope.addUserToTeam = function(user) {
+    client.action('addUserToGame', {user: user.id, game: $scope.selectedGame.id}, function(err, data) {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        client.action('addUserToTeam', {
+          user: user.id,
+          game: $scope.selectedGame.id,
+          team: $scope.currentTeam.id
+          }, function(err, data) {
+            if (data.error) {
+              console.log(data.error);
+            } else {
+              $('#addUserModal').modal('hide');
+              $scope.selectedGame.teams = data.game.teams;
+              $scope.getGames();
+            }
+          });
+      }
+    });
+  };
+
+  $scope.removeUserFromTeam = function(user) {
+    client.action('removeUserFromTeam', {
+      user: user.id,
+      game: $scope.selectedGame.id
+      }, function(err, data) {
+        if (data.error) {
+          console.log(data.error);
+        } else {
+          client.action('removeUserFromGame', {user: user.id, game: $scope.selectedGame.id}, function(err, data) {
+            if (data.error) {
+              console.log(data.error);
+            } else {
+              $scope.selectedGame.teams = data.game.teams;
+              $scope.getGames();
+            }
+          });
+        }
+      });
+  };
 
   $scope.clearSelectedGame = function() {
     $scope.selectedGame = null;
     $scope.game = null;
   };
+
+  $scope.startGame = function() {
+    if ($scope.selectedGame) {
+      client.action('startGame', {id: $scope.selectedGame.id}, function(err, data) {
+        if (data.error) {
+          console.log(data.error);
+        } else {
+          $scope.selectedGame.startedAt = data.game.startedAt;
+          $scope.$digest();
+        }
+      });
+    }
+  }
+
+  $scope.selectUser = function(userId) {
+    $scope.userId = userId;
+  }
+
+  $scope.currentUser = function(userId) {
+    return $scope.userId === userId;
+  }
 
   $scope.getGames = function() {
     client.action('listGames', {}, function(err, data) {
@@ -24,8 +143,15 @@ angular.module('adminControllers').controller('GameListCtrl', ['$scope', functio
 
   $scope.selectGame = function(game) {
     if (!$scope.selectedGame || $scope.selectedGame.id !== game.id) {
-      $scope.selectedGame = jQuery.extend(true, {}, game);
-      $scope.game = jQuery.extend(true, {}, game);
+      client.action('showGame', {id: game.id}, function(err, data) {
+        if (data.error) {
+          console.log(data.error);
+        } else {
+          $scope.selectedGame = jQuery.extend(true, {}, data.game);
+          $scope.game = jQuery.extend(true, {}, data.game);
+          $scope.$digest();
+        }
+      });
     }
   };
 
