d3fbb5b5418754a707147d8e3e7efcc8d609f26a
Jan van de Kerkhof <vdkerkhof.j@gmail.com>
2015-04-09 14:49:30 +0300

    Add gameflow functionality

--- controller.js
+++ controller.js
@@ -19,6 +19,12 @@ var options = {
 
 var api = null;
 
+var maxBrightness = 255;
+var minBrightness = 40;
+var answerFlickers = 4;
+var lengthOfFlicker = 1000;
+var currentTeams = null;
+
 var log = function(res) {
   console.log(res);
   return res;
@@ -34,33 +40,180 @@ var shutdown = function(err) {
   });
 };
 
+var setLightState = function(light, state) {
+  if (light === 0) {
+    api.setGroupLightState(0, state)
+      .then(function() {
+        console.log('Updated lights to state', state._values);
+      })
+      .fail(function(err) {
+        console.log('Error:', err);
+      });
+  } else {
+    api.setLightState(light, state)
+    .then(function() {
+      console.log('Updated light ' + light + ' to state', state._values);
+    })
+    .fail(function(err) {
+      console.log('Error:', err);
+    });
+  }
+};
+
+var setTeamLight = function(team, color, brightness) {
+  var light = team === 0 ? 1 : 3; //go to first or third light
+  brightness = typeof brightness !== 'undefined' ? brightness : maxBrightness;
+  var state = createState();
+  state.rgb(color);
+  state.bri(brightness);
+  // state.transitionSlow();
+  setLightState(light, state);
+};
+
+var setTeamScores = function(teams) {
+  clearTimeouts();
+  currentTeams = teams;
+  var maxTeamIdx = teams[1].score > teams[0].score ? 1 : 0;
+  var minTeamIdx = (maxTeamIdx + 1) % 2;
+  var max = teams[maxTeamIdx].score;
+  var min = teams[minTeamIdx].score;
+  var minBright = (maxBrightness - minBrightness) * (min / max) + minBrightness;
+  setTeamLight(minTeamIdx, teams[minTeamIdx].color, minBright);
+  setTeamLight(maxTeamIdx, teams[maxTeamIdx].color);
+  var state = createState(); //set middle light to white
+  state.rgb([255, 255, 255]);
+  state.bri(minBrightness);
+  setLightState(2, state);
+};
+
+var clearTimeouts = function() {
+  for (var i = 0; i < timeouts.length; i++) {
+    clearTimeout(timeouts[i]);
+  }
+};
+
+var answer = function(correct) {
+  clearTimeouts();
+
+  var state = createState();
+  if (correct) {
+    setAll(146, 100, 0); //green
+  } else {
+    setAll(356, 100, 0); //red
+  }
+  state.alertShort();
+  var flicker = function() {setLightState(0, state);};
+
+  for (var i = 1; i <= answerFlickers; i++) {
+    timeouts.push(setTimeout(flicker, i * lengthOfFlicker));
+  }
+
+  timeouts.push(setTimeout(function() {
+    if (currentTeams) {
+      setTeamScores(currentTeams);
+    }
+  }, (answerFlickers + 1) * lengthOfFlicker));
+};
+
+var gameOver = function() {
+  var state = createState();
+  state.colorLoop();
+  state.bri(maxBrightness);
+  setLightState(0, state);
+};
+
+var reset = function() {
+  var state = createState();
+  state.reset();
+  state.effect('none');
+  setLightState(0, state);
+};
+
+var timeouts = [];
+
+var countDown = function(seconds) {
+  clearTimeouts();
+  var state = createState();
+  state.rgb([0, 255, 0]);
+  state.bri(maxBrightness);
+  setLightState(2, state);
+
+  var flicker = function(i) {
+    var percentageDone = i / seconds;
+    var rgb = [percentageDone * 255, (1 - percentageDone) * 255, 0];
+    var state = createState();
+    state.rgb(rgb);
+    state.alertShort();
+    setLightState(2, state); //middle light
+  };
+  for (var i = 1; i <= seconds; i++) {
+    timeouts.push(setTimeout(flicker, i * lengthOfFlicker, i));
+  }
+  timeouts.push(setTimeout(answer, (seconds) * lengthOfFlicker, false));
+};
+
+var setAll = function(h, s, l) {
+  var state = createState();
+  state.hsl(h, s, l);
+  state.bri(maxBrightness);
+  setLightState(0, state);
+};
+
 var startServerConnection = function() {
 
   sociaLights.on('say', function(msgBlock) {
-    if (msgBlock.from === 0 && msgBlock.message.cmdType === 'HUECMD') {
+    if (msgBlock.from === 0) {
       var state = createState();
-      msgBlock.message.state.forEach(function(modifier) {
-        var fun = modifier.shift();
-        state[fun].apply(state, modifier);
-      });
+      switch (msgBlock.message.cmdType) {
+        case 'HUECMD':
+          msgBlock.message.state.forEach(function(modifier) {
+            var fun = modifier.shift();
+            state[fun].apply(state, modifier);
+          });
+          if (msgBlock.message.to) {
+            api.setLightState(msgBlock.message.to, state)
+              .then(function() {
+                console.log('Updated light ' + msgBlock.message.to + ' to state', state._values);
+              })
+              .fail(function(err) {
+                console.log('Error:', err);
+              });
+          } else {
+            api.setGroupLightState(0, state)
+              .then(function() {
+                console.log('Updated lights to state', state._values);
+              })
+              .fail(function(err) {
+                console.log('Error:', err);
+              });
+          }
+          break;
 
-      if (msgBlock.message.to) {
-        api.setLightState(msgBlock.message.to, state)
-          .then(function() {
-            console.log('Updated light ' + msgBlock.message.to + ' to state', state._values);
-          })
-          .fail(function(err) {
-            console.log('Error:', err);
-          });
-      } else {
-        api.setGroupLightState(0, state)
-          .then(function() {
-            console.log('Updated lights to state', state._values);
-          })
-          .fail(function(err) {
-            console.log('Error:', err);
-          });
+        case 'SETTEAM':
+          var team = msgBlock.message.team;
+          var color = msgBlock.message.color;
+          setTeamLight(team, color);
+          break;
+        case 'SETSCORES':
+          console.log('setting scores');
+          var teams = msgBlock.message.teams;
+          setTeamScores(teams);
+          break;
+        case 'ANSWER':
+          answer(msgBlock.message.correct);
+          break;
+        case 'STARTCOUNTDOWN':
+          countDown(msgBlock.message.seconds);
+          break;
+        case 'GAMEOVER':
+          gameOver();
+          break;
+        case 'NEWGAME':
+          reset();
+          break;
+
       }
+
     }
   });
 
---
+++ sample.js
@@ -0,0 +1,1 @@
+api.chatRoom.broadcast({}, 'room:demo', {cmdType: 'ANSWER', correct: false})
